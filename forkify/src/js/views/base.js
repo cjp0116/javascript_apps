7c7407e5f0405ddb16efd6807dfe10edd86953cb
Jae Pil Cho <48457791+cjp0116@users.noreply.github.com>
2019-08-29 11:39:45 -0700

    Add files via upload

--- forkify/src/js/views/base.js
+++ forkify/src/js/views/base.js
@@ -1,22 +1,24 @@
-// Create an object from our DOM.
 export const elements = {
-    searchInput: document.querySelector(".search__field"),
-    searchForm: document.querySelector(".search"),
-    searchResList: document.querySelector(".results__list"),
-    searchRes: document.querySelector(".results"),
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
+    searchResList: document.querySelector('.results__list'),
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list'),
+    likesMenu: document.querySelector('.likes__field'),
+    likesList: document.querySelector('.likes__list')
 };
 
 export const elementStrings = {
-    loader: 'loader',
-
+    loader: 'loader'
 };
 
-// Create the spinners
 export const renderLoader = parent => {
     const loader = `
-        <div class=${elementStrings.loader}>
+        <div class="${elementStrings.loader}">
             <svg>
-                <use href= "img/icons.svg#icon-cw"></use>
+                <use href="img/icons.svg#icon-cw"></use>
             </svg>
         </div>
     `;
@@ -25,5 +27,5 @@ export const renderLoader = parent => {
 
 export const clearLoader = () => {
     const loader = document.querySelector(`.${elementStrings.loader}`);
-    if(loader) loader.parentElement.removeChild(loader);
+    if (loader) loader.parentElement.removeChild(loader);
 };
